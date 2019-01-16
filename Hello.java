e371d035fc1a551bd5b7c67a35a7723c2912fe6a
WTremaglio <46757211+WTremaglio@users.noreply.github.com>
2019-01-16 12:20:34 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,24 @@
+/*************************************************************************
+ * Java Program Name
+ *************************************************************************
+ * Project/Program description
+ * _______________________________________________________________________
+ * Student First Name Last Name
+ * Date
+ * Course Number and Section
+ *************************************************************************/
+ 
+ public class Hello {
+  public static void printHeading() {
+   System.out.prinln("William Tremaglio");
+   System.out.println("Hello Java");
+   System.out.println("Project Description");
+   System.out.println("Version Date");
+   System.out.println("CMSC255 04");
+   System.out.println();
+  }
+  public static void main(String [] args) {
+   printHeading();
+   System.out.println("Hello");
+  }
+ }
